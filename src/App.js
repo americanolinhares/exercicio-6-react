43b48046774f025cb7e197c6c405deaccc296c49
Frederico Americano <frederico@take.net>
2020-04-14 23:48:30 -0300

    Add correct files

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   
   return (
     <div className="App">
-      <h1>Exercício 4 - Filmes</h1>
+      <h1>Exercício 3 - Filmes</h1>
       <MoviesList />
     </div>
   );
---
+++ src/NotFound.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react';
+
+class NotFound extends Component{
+
+    render(){
+        return(            
+            <Fragment>
+                <div className="container">
+                    <h1 className="center"> Página não encontrada </h1>
+                </div>               
+            </Fragment>            
+        )
+    }
+
+}
+export default NotFound;
